7039806542e03bd2cf5ecc8d377baadb82c17cbe
348818030 <348818030@gapps.yrdsb.ca>
2026-04-20 11:23:54 -0400

    Added Banana.java class

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Banana obstacle = new Banana();
+        addObject(obstacle, 300, 200);
     }
 }
