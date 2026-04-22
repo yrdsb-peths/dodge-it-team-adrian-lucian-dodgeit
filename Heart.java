95a58706b5df28ca1ff1bce62ca25e93e25363aa
348818030 <348818030@gapps.yrdsb.ca>
2026-04-22 11:31:48 -0400

    Add more hearts, and lifeCount class variable

--- Heart.java
+++ Heart.java
@@ -4,11 +4,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Heart extends Actor
 {
     private static GreenfootImage myImage;
+    public static int lifeCount = 0;
     
     public Heart()
     {
         myImage = new GreenfootImage("herz.png");
         setImage(myImage);
+        
+        lifeCount++;
     }
     
     public void act()
--- MyWorld.java
+++ MyWorld.java
@@ -10,6 +10,10 @@ public class MyWorld extends World {
         addObject(hero, 100, 100);
         
         Heart life1 = new Heart();
-        addObject(life1, 100, 350);
+        addObject(life1, 100, 370);
+        Heart life2 = new Heart();
+        addObject(life2, 200, 370);
+        Heart life3 = new Heart();
+        addObject(life3, 300, 370);
     }
 }
