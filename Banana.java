4e03986e94b9c0ede184a137cdf543254fee831c
348818030 <348818030@gapps.yrdsb.ca>
2026-04-22 11:17:34 -0400

    Banana now moves

--- Banana.java
+++ Banana.java
@@ -18,6 +18,19 @@ public class Banana extends Actor
     
     public void act()
     {
-        // Add your action code here.
+        move(-8);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
