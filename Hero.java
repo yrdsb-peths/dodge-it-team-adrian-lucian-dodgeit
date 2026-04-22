4c1b76db02c78662a3309eb50796db99683d7fe6
348818030 <348818030@gapps.yrdsb.ca>
2026-04-22 11:09:19 -0400

    Fix hero image not appearing

--- Hero.java
+++ Hero.java
@@ -1,13 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Hero extends Actor
 {
+    private static GreenfootImage myImage;
+    
+    public Hero()
+    {
+        myImage = new GreenfootImage("ppl1.png");
+        setImage(myImage);
+    }
+    
     boolean atTop = true;
     public void act()
     {
