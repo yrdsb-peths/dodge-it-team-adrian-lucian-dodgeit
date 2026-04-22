fb3b87a10e47546af50959f02eff1f1e6a3fdd0d
Wisulve-328 <440042792@gapps.yrdsb.ca>
2026-04-22 11:29:51 -0400

    add a sadface object

---
+++ Sadface.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Sadface here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sadface extends Actor
+{   
+    private static GreenfootImage myImage;
+    
+    public Sadface() {
+        myImage = new GreenfootImage("smiley3.png");
+        setImage(myImage);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
