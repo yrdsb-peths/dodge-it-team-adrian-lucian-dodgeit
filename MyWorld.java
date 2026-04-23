65dbafa8e03896975f30c071ae095e68800a444a
348818030 <348818030@gapps.yrdsb.ca>
2026-04-23 17:29:19 -0400

    Update banana spawn place

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         Banana obstacle = new Banana();
-        addObject(obstacle, 300, 200);
+        addObject(obstacle, 600, 200);
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
